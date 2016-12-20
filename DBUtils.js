244da6b12f33d91e8eed34ac38d742202cfd40e0
laisf <sanfa.lai@partner.midea.com.cn>
2016-12-20 09:13:24 +0800

    增加数据库连接demo

--- DBUtils.js
+++ DBUtils.js
@@ -9,7 +9,7 @@ console.log(dbConfig);
 /**
  * 连接
  */
-var db = mongoose.connect(dbConfig.dbUrl,options);
+var db = mongoose.connect(dbConfig.dbUrl, options);
 
 mongoose.connection.once('connected', function() {
 	console.log('Mongoose connection open to ' + dbConfig.dbUrl);
@@ -28,3 +28,52 @@ mongoose.connection.on('error', function(err) {
 mongoose.connection.on('disconnected', function() {
 	console.log('Mongoose connection disconnected');
 });
+
+var mongooseSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		default: '匿名用户'
+	},
+	title: {
+		type: String
+	},
+	content: {
+		type: String
+	},
+	time: {
+		type: Date,
+		default: Date.now
+	},
+	age: {
+		type: Number
+	}
+});
+
+var mongooseModel = db.model('mongoose', mongooseSchema);
+
+var doc = {
+	username: 'emtity_demo_username',
+	title: 'emtity_demo_title',
+	content: 'emtity_demo_content'
+};
+
+// new mongooseModel(doc).save(function (error) {
+//     if(error) {
+//         console.log(error);
+//     } else {
+//         console.log('saved OK!');
+//     }
+//     // 关闭数据库链接
+//     db.close();
+// })
+console.log(db)
+var disconnect = function() {
+	db.disconnect(function(error) {
+		if (error) {
+			throw error;
+		}
+		console.log("close successful")
+	});
+}
+
+disconnect();
